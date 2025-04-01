f721dc06285e8ffd3aae95908a3ac1f1c09b67af
Paul Yang <pyang@conversantmedia.com>
2025-04-01 10:37:38 -0700

    MPUB-20026 CitrusAd banner test page - Add scenarios

---
+++ docs/citrusad/scenarios.js
@@ -0,0 +1,120 @@
+(function() {
+    const scenarios = [
+        {
+            name: "Search placement",
+            data: {
+                customerId: "wertg5432a",
+                sessionId: "ec9-4e07-881d-3e9",
+                dtmCookieId: "AAAF8xLBTA968AB6TOthAAAAAAE",
+                placement: "search",
+                catalogId: "628dbe95-2ec9-4e07-881d-3e9f92ab2e0b",
+                searchTerm: "chocolate",
+                options: {
+                    filterMode: "AndOr"
+                },
+                contentStandardId: "fec2ab89-7a29-42b5-b58a-5675688b52d9",
+                bannerSlots: [
+                    {
+                        slotId: "<SLOT_ID>",
+                        maxNumberOfAds: 1
+                    },
+                    {
+                        slotId: "<SLOT_ID>",
+                        maxNumberOfAds: 2
+                    }
+                ],
+                maxNumberOfAds: 3
+            }
+        },
+        {
+            name: "Category placement",
+            defaultSelected: true,
+            data: {
+                customerId: "wertg5432a",
+                sessionId: "ec9-4e07-881d-3e9",
+                dtmCookieId: "AAAF8xLBTA968AB6TOthAAAAAAE",
+                placement: "category",
+                catalogId: "628dbe95-2ec9-4e07-881d-3e9f92ab2e0b",
+                productFilters: [
+                    [
+                        "category:Cupboard/Snacks"
+                    ]
+                ],
+                options: {
+                    filterMode: "AndOr"
+                },
+                contentStandardId: "fec2ab89-7a29-42b5-b58a-5675688b52d9",
+                bannerSlots: [
+                    {
+                        slotId: "<SLOT_ID>",
+                        maxNumberOfAds: 1
+                    },
+                    {
+                        slotId: "<SLOT_ID>",
+                        maxNumberOfAds: 2
+                    }
+                ],
+                maxNumberOfAds: 3
+            }
+        },
+        {
+            name: "Cross-sell category placement",
+            data: {
+                customerId: "wertg5432a",
+                sessionId: "ec9-4e07-881d-3e9",
+                dtmCookieId: "AAAF8xLBTA968AB6TOthAAAAAAE",
+                placement: "category-cross-sell",
+                catalogId: "628dbe95-2ec9-4e07-881d-3e9f92ab2e0b",
+                productFilters: [
+                    [
+                        "category:Cupboard/Snacks"
+                    ]
+                ],
+                options: {
+                    filterMode: "AndOr"
+                },
+                contentStandardId: "fec2ab89-7a29-42b5-b58a-5675688b52d9",
+                bannerSlots: [
+                    {
+                        slotId: "<SLOT_ID>",
+                        maxNumberOfAds: 1
+                    },
+                    {
+                        slotId: "<SLOT_ID>",
+                        maxNumberOfAds: 2
+                    }
+                ],
+                maxNumberOfAds: 3
+            }
+        },
+        {
+            name: "Broad match placement",
+            data: {
+                customerId: "wertg5432a",
+                sessionId: "ec9-4e07-881d-3e9",
+                dtmCookieId: "AAAF8xLBTA968AB6TOthAAAAAAE",
+                placement: "home",
+                catalogId: "628dbe95-2ec9-4e07-881d-3e9f92ab2e0b",
+                productFilters: [
+                    []
+                ],
+                options: {
+                    filterMode: "AndOr"
+                },
+                contentStandardId: "fec2ab89-7a29-42b5-b58a-5675688b52d9",
+                bannerSlots: [
+                    {
+                        slotId: "<SLOT_ID>",
+                        maxNumberOfAds: 1
+                    },
+                    {
+                        slotId: "<SLOT_ID>",
+                        maxNumberOfAds: 2
+                    }
+                ],
+                maxNumberOfAds: 3
+            }
+        }
+    ];
+    window.citrusAdScenarios = scenarios;
+})();
